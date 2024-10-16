2988f8e39ca8eba73ace0825703f472fa63e201c
kailahgayle <kailahkg@gmail.com>
2024-10-15 20:25:05 -0400

    Home Screen in the process of applying ledger

---
+++ AccountingLedgerApp/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,37 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> ledger = new ArrayList<>(); //for ledger transactions
+        //Home Screen Options
+        while (true) {
+            System.out.println("Home Screen");
+            System.out.println("D) Add Deposit");
+            System.out.println("P) Make Payment (Debit)");
+            System.out.println("L) Ledger")
+            System.out.println("X) Exit");
+            System.out.println("Please Enter a Choice");
+
+        // Prompt for user input
+            String choice = scanner.nextLine().toUpperCase();
+
+        //Results after user input
+            if (choice.equals("D")) {
+                System.out.println("You chose Add Deposit.");
+            } else if (choice.equals("P")) {
+                System.out.println("You chose Make Payment (Debit) ");
+            } else if (choice.equals("X")) {
+                System.out.println("Exiting....");
+                break; // Exit loop
+            } else {
+                System.out.println("Invalid, please select another option");
+            }
+        }
+        scanner.close();
+        }
+    }
