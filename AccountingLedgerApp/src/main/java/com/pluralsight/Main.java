cfc5a606c4ee9caeb7ac70f5ce7c36ce837f8875
kailahgayle <kailahkg@gmail.com>
2024-10-16 11:12:23 -0400

    Home Screen update, before ledger

--- AccountingLedgerApp/src/main/java/com/pluralsight/Main.java
+++ AccountingLedgerApp/src/main/java/com/pluralsight/Main.java
@@ -1,19 +1,16 @@
 package com.pluralsight;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<String> ledger = new ArrayList<>(); //for ledger transactions
+
         //Home Screen Options
         while (true) {
             System.out.println("Home Screen");
             System.out.println("D) Add Deposit");
             System.out.println("P) Make Payment (Debit)");
-            System.out.println("L) Ledger")
             System.out.println("X) Exit");
             System.out.println("Please Enter a Choice");
 
@@ -23,6 +20,7 @@ public class Main {
         //Results after user input
             if (choice.equals("D")) {
                 System.out.println("You chose Add Deposit.");
+                System.out.println(".");
             } else if (choice.equals("P")) {
                 System.out.println("You chose Make Payment (Debit) ");
             } else if (choice.equals("X")) {
