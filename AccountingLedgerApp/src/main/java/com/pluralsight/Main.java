39f32d468c264106a30324f6097ba1248f03cfca
kailahgayle <kailahkg@gmail.com>
2024-10-17 03:56:47 -0400

    Ledger was added with T,D,P,R

--- AccountingLedgerApp/src/main/java/com/pluralsight/Main.java
+++ AccountingLedgerApp/src/main/java/com/pluralsight/Main.java
@@ -3,12 +3,15 @@ package com.pluralsight;
 import java.util.Scanner;
 
 public class Main {
-    private static final String dataFileName = "transactions.csv";
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        //Where home screen starts
+        showHomeScreen(scanner);
+    }
+
         //Home Screen Options
-        while (true) {
+        private static void showHomeScreen(Scanner scanner) {
             System.out.println("Home Screen");
             System.out.println("D) Add Deposit");
             System.out.println("P) Make Payment (Debit)");
@@ -32,20 +35,64 @@ public class Main {
                     break;
                 case "X":
                     System.out.println("Exiting..");
-                    return; //this will exit the loop
+                    System.exit(0); //this will exit the loop
+                    break;
                 default:
                     System.out.println("Invalid option. Please try Again.");
+                    showHomeScreen(scanner); // this added scanner would let you stay in home screen if its an invalid option
             }
         }
-    }
 
-    // ledger screen will go here
+    // for deposits and payments
     private static void addTransaction(Scanner scanner, String type) {
-    //
+    //trn  hh
     System.out.println(type + "option selected.");
+    showHomeScreen(scanner); // this will go back to Home Screen  after adding the transaction
 }
+
+// Ledger
     private static void showLedgerScreen(Scanner scanner) {
-    //
-   System.out.println("Ledger screen selected.");
+        System.out.println("Ledger screen selected.");
+        System.out.println("A) All transactions");
+        System.out.println("D) Deposits");
+        System.out.println("P) Payments");
+        System.out.println("R) Reports");
+        System.out.println("H) Home");
+        System.out.println("D) Please Enter a Choice:");
+
+        String choice = scanner.nextLine().toUpperCase();
+
+        switch (choice) {
+            case "A":
+                displayLedger("All");
+                break;
+            case "D":
+                displayLedger("DEPOSITS");
+                break;
+            case "P":
+                displayLedger("PAYMENTS");
+                break;
+            case "R":
+                showReportsScreen(scanner);
+                break;
+            case "H":
+                showHomeScreen(scanner); //back to Home Screen
+                break;
+            default:
+                System.out.println("Invalid option. Please try Again.");
+                showLedgerScreen(scanner); // this ensures that you will stay in Ledger
+        }
+    }
+// this will display the Ledger
+    private static void displayLedger(String filter) {
+        //transactions will be displayed here with filter
+        System.out.println("Displaying" + filter + "transactions");
+        showLedgerScreen(new Scanner(System.in));
+    }
+// this will show reports
+    private static void showReportsScreen(Scanner scanner) {
+        //reports screen method
+        System.out.println("Reports screen selected.");
+        showLedgerScreen(scanner);
     }
 }
